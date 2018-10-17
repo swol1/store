08867d37a6c5d65babb1943184bcfc3246395b0a
swol1 <den.gavrilin1@gmail.com>
2018-10-17 18:25:47 +0300

    version 0.2, added read_from_file method

--- main.rb
+++ main.rb
@@ -1,22 +1,11 @@
-require_relative "product"
-require_relative "book"
-require_relative "film"
+require_relative "lib/product"
+require_relative "lib/book"
+require_relative "lib/film"
 
-products = []
+current_path = File.dirname(__FILE__)
 
-products << Film.new(price: 490, amount: 15, name: "Леон", year: 1994, author: "Люк Бессон")
-products << Film.new(price: 720, amount: 7, name: "Дурак", year: 2014, author: "Юрий Быков")
+film = Film.from_file("#{current_path}/data/films/01.txt")
+book = Book.from_file("#{current_path}/data/books/01.txt")
 
-products << Book.new(price: 1024, amount: 25, name: "Идиот", genre: "роман", author: "Федор Достоевский")
-products << Book.new(price: 189, amount: 4, name: "Цветы для Элджернона", genre: "научная фантастика", author: "Даниел Киз")
-
-products.each { |product| puts product }
-
-book = Book.new(price: "1024", amount: "25", name: "Идиот")
-
-book.author = "some author"
-
-book.update(author: "Федор Достоевский", genre: "роман", price: "3434")
-
-puts "Вот какие товары у нас есть"
+puts film
 puts book
